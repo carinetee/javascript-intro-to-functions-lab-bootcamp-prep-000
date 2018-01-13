07892fbcfeb679c39011829f1eb48e7316cc5811
Carine Tomassian <carinetomassian@gmail.com>
2018-01-13 03:35:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === lowercase) {
     return "I can\'t hear you!"
-  } else if string === "I love you, Grandma." {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   } else {
     return "YES INDEED!"
